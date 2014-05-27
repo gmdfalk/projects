e9e6c23c0b30e6ddc27af19cac77be0447d15ad1
max demian <mikar@gmx.de>
2014-05-27 19:59:35 +0200

    derping around

--- javaweek/src/grundkurs/InputOutput.java
+++ javaweek/src/grundkurs/InputOutput.java
@@ -33,28 +33,74 @@ public class InputOutput {
 
 	public static void inputAlt() {
 		int zip = 0;
+		double num = 0;
+		String city, flavor, output;
 		Scanner keyboard = new Scanner(System.in);
-		System.out.print("What city do you live in?");
-		String city = keyboard.next();
-		System.out.print("Whats your zipcode?");
-		try {
+		System.out.println("What city do you live in? ");
+		city = keyboard.next();
+		System.out.println("Whats your zipcode? ");
+		if (keyboard.hasNextInt()) {
 			zip = keyboard.nextInt();
-		} catch (InputMismatchException e) {
-			// skip
-			// System.out.print("Invalid input!");
 		}
-		System.out.print("A number between 0 and 1?");
-		double num = keyboard.nextDouble();
-		System.out.print("Your favorite ice cream flavor?");
-		String flavor = keyboard.next();
-		String output = "Hey, you are from %s (%s), the number was %s and your favorite ice cream is %s!";
+		keyboard.next();
+		System.out.println("A number between 0 and 1? ");
+		if (keyboard.hasNextDouble() && keyboard.nextDouble() <= 1.0) {
+			num = keyboard.nextDouble();
+		}
+		keyboard.next();
+		System.out.println("Your favorite ice cream flavor? ");
+		flavor = keyboard.next();
+		output = "Hey, you are from %s (%s), the number was %s and your favorite ice cream is %s!";
 		System.out.println(String.format(output, city, zip, num, flavor));
+		keyboard.close();
+	}
+
+	public static void nums() {
+		int a = 1, b = 2, c = 3, d = 4;
+		System.out.println(++a); // 2
+		System.out.println(a); // 2
+		System.out.println(b++); // 2
+		System.out.println(b); // 3
+		System.out.println((++c) + (++c)); // 9
+		System.out.println(c); // 5
+		System.out.println((d++) + (d++)); // 9
+		System.out.println(d); // 6
+	}
+
+	public static double add() {
+		double x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6;
+		x1 = 1e10;
+		x2 = 1223;
+		x3 = 1e18;
+		x4 = 1e15;
+		x5 = 3;
+		x6 = -1e12;
+		y1 = 1e20;
+		y2 = 2;
+		y3 = -1e22;
+		y4 = 1e13;
+		y5 = 2111;
+		y6 = 1e16;
+		return x1 * y1 + x2 * y2 + x3 * y3 + x4 * y4 + x5 * y5 + x6 * y6;
+	}
+
+	public static double paren() {
+		double x = 192119201;
+		double y = 35675640;
+		double x2 = x * x;
+		double y2 = y * y;
+		double s = (1682 * x * y2 * y2 + 3 * x2 * x + 29 * x * y2 - 2 * x2 * x2
+				* x + 832);
+		return s * (1.0 / 107751.0);
 	}
 
 	public static void main(String[] args) {
 		// toolsInput();
 		// input();
-		inputAlt();
+		// inputAlt();
+		// nums();
+		System.out.println(add());
+		System.out.println(paren());
 	}
 
 	private static void toolsInput() {
